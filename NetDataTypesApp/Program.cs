dd4aeb37ea8263a75b15664a1836afe48e0f42b7
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-08-14 21:16:22 +0300

    Добавьте файлы проекта.

---
+++ NetDataTypesApp/Program.cs
@@ -0,0 +1,33 @@
+﻿//Console.WriteLine("Hello, World!");
+
+//bool f1, f3;
+//Boolean f2;
+
+//f1 = true;
+//f2 = false;
+//f3 = 20 > 0;
+
+//byte b1 = 0xFF;
+//Byte b2 = 0b11011011;
+
+//Console.WriteLine(byte.MaxValue);
+
+//int n = 525;
+//byte m = (byte)n;
+//Console.WriteLine(m);
+
+//string snum = "-100";
+//int num = Int32.Parse(snum);
+//Console.WriteLine(int.Abs(num));
+
+//int innum = Int32.Parse(Console.ReadLine());
+
+//double x = Double.Parse(Console.ReadLine());
+
+int a = 10;
+int b = 20;
+
+if(a < 0 || ++b > 0)
+    Console.WriteLine("way 1");
+
+Console.WriteLine(b);
